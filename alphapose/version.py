0edef48bd9e3cd6105681f0bcd84c50521f6747b
phelogges <wukun3269@qq.com>
2022-09-26 08:55:35 +0800

    custom version and comments

--- alphapose/version.py
+++ alphapose/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Thu Jul 21 17:10:51 2022
+# TIME: Sun Sep 25 14:53:15 2022
 
-__version__ = '0.6.0+29ace8c'
-short_version = '0.6.0'
+__version__ = '0.5.0+308cc9a'
+short_version = '0.5.0'
